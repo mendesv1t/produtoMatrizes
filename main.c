784844032fa0ca7b2420f972aeb663af84666f7c
Vitória Chaves <vitoriagrier@gmail.com>
2024-04-10 19:59:37 -0300

    corrige método de produto de matrizes para qualquer dimensão

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include "threads.c"
 int main(void) {
 
-    /*toDo:
-     * - criar método sequencial de produto de matrizes (em andamento)
+/*toDo:
+     * - criar método sequencial de produto de matrizes (check)
      * - criar tarefa das threads
      * - criar threads
      * - estimar tempo se fosse realizado com um código sequencial para entradas muito grandes e extrair a matriz de saída
@@ -11,4 +11,23 @@ int main(void) {
      * - Para as entradas de dimensoes:  500X500, 1000X1000 e 2000X2000. Repetir 3 vezes cada e realizar a média de tempo.
      * - estimar aceleração e eficiência com 1, 2, 4 e 8 threads
      */
+
+
+    int linhasA = 3, colunasA = 3;
+    int linhasB = 3, colunasB = 1;
+
+    float matrizA[] = {1, 2, 3,3, 4,5,5,6,6};
+    float matrizB[] = {1,2,3};
+
+    /*
+     * [1 2 3] [1]
+     * [3,4 5] [2]
+     * [5,6 6] [3]
+     */
+
+    matrizC = produtoMatrizesSequencial(matrizA, matrizB, colunasA, linhasA, colunasB, linhasB);
+    if (matrizC != NULL) {
+        free(matrizC);
+    }
+
 }
--- metodos_vetor.c
+++ metodos_vetor.c
@@ -7,24 +7,29 @@ void * produtoMatrizes() {
     return NULL;
 }
 
-float * produtoMatrizesSequencial(float * matrizA, float * matrizB, int colunasA, int linhasB) {
-
+float * produtoMatrizesSequencial(float * matrizA, float * matrizB, int colunasA, int linhasA, int colunasB, int linhasB) {
     if (colunasA != linhasB) {
         printf("Erro: número de colunas da matriz A não é igual ao número de linhas da matriz B.");
         return NULL;
     }
 
-    matrizC = (float *) malloc(sizeof(float) * colunasA * linhasB);
-    for (int i = 0; i < colunasA; i++) {
-        for (int j = 0; j < linhasB; j++) {
-            matrizC[i*linhasB + j] = 0;
+    matrizC = (float *) malloc(sizeof(float) * linhasA * colunasB);
+    if (matrizC == NULL) {
+        printf("Erro ao alocar memória para a matriz C.");
+        return NULL;
+    }
+
+    for (int i = 0; i < linhasA; i++) {
+        for (int j = 0; j < colunasB; j++) {
+            matrizC[i * colunasB + j] = 0;
             for (int k = 0; k < colunasA; k++) {
-                matrizC[i*linhasB + j] += matrizA[k*colunasA + j]*matrizB[i*linhasB + k];
+                matrizC[i * colunasB + j] += matrizA[i * linhasA + k] * matrizB[k * colunasB + j];
             }
-            printf("%f ", matrizC[i*linhasB + j]);
+            printf("%f ", matrizC[i * colunasB + j]);
         }
         printf("\n");
     }
     return matrizC;
 }
+
 
